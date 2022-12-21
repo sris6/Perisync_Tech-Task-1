2746e72c53420664ef15e0e1d961fd0a6a6bd924
Srikanth V <118743187+sris6@users.noreply.github.com>
2022-12-21 18:23:52 +0530

    Add files via upload

---
+++ Task 7/script.js
@@ -0,0 +1,31 @@
+
+let profileEl = document.getElementById("profile-opts");
+let addGuestEl = document.getElementById("add-guests-opts");
+let anyWeekEl = document.getElementById("any-week-opts");
+let anyWhereEl = document.getElementById("any-where-opts");
+
+let showHide = 0;
+
+function profileShowHide() {
+    profileEl.classList.toggle("hide");
+}
+
+
+function addGuestShowHide() {
+    addGuestEl.classList.toggle("hide");
+}
+
+
+function anyWhereShowHide() {
+    anyWhereEl.classList.toggle("hide");
+}
+
+
+function anyWeekShowHide() {
+    anyWeekEl.classList.toggle("hide");
+}
+
+
+
+
+
